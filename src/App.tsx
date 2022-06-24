f7ea249e24d16ab0fb892c001d31db08c159645b
SzymonGonerko <szymon.gonerko@gmail.com>
2022-06-24 15:38:30 +0200

    initial canvas environment, added TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import {Html, OrbitControls, Environment} from '@react-three/drei'
+
+import { ToDoList } from './components/ToDoList';
+
+
 
 const App = () => {
-  return (
-    <div className="App">
 
-    </div>
+
+  return (
+<Canvas dpr={[30, 2]} camera={{ position: [-Math.PI/2, 0, 0] } }>
+  
+      <Suspense fallback={null}>
+      
+          <OrbitControls/>
+          <Environment preset="sunset" background />
+
+          <group position={[7,0,0]} rotation={[0,-Math.PI/2,0]} >
+            <Html transform>
+              <ToDoList/>
+            </Html>
+          </group>
+
+
+      </Suspense>
+</Canvas>
   );
 }
 
